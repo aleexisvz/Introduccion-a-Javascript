8176035b3e1c3f65f1bd9ce370cf1b5b50782183
Alexis Vazquez <alexiis1998@hotmail.com>
2023-01-30 20:31:28 -0300

    Leccion 6.5 SOLUCIONADO

---
+++ Leccion 6.5/main.js
@@ -0,0 +1,20 @@
+class Evento {
+   constructor(id, nombre, cantidadAsistentes, precioPorAsistente) {
+      this.id = id;
+      this.nombre = nombre;
+      this.cantidadAsistentes = cantidadAsistentes;
+      this.precioPorAsistente = precioPorAsistente;
+   }
+
+   enlace() {
+      return '/evento/' + this.id;
+   }
+
+   dineroRecaudado() {
+      return this.cantidadAsistentes * this.precioPorAsistente;
+   }
+}
+
+var evento1 = new Evento(1,"e1", 12, 5);
+console.log(evento1.enlace())
+console.log(evento1.dineroRecaudado())
