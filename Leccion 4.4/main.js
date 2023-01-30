66dda6cf433b371d6053089cdb5485b3c1eaac13
Alexis Vazquez <alexiis1998@hotmail.com>
2023-01-30 18:48:30 -0300

    Leccion 4.5 SOLUCIONADA

--- Leccion 4.4/main.js
+++ Leccion 4.4/main.js
@@ -1,7 +1,7 @@
 let canciones = ["Franky Rizardo - The Floor is Lava", "KoRn - Blind", "Rise Against - Savior", 
 "Rise Against - Swing Life Away", "Rise Against - Hero of War", "Rise Against - Prayer of the Refugee"];
 
-function imprimirCanciones() {
+function imprimirCanciones(canciones) {
    console.log("Mi lista de canciones preferidas: ");
     for (let i = 0; i < canciones.length; i++) {
       console.log(canciones[i] + " ");
---
+++ Leccion 4.5/main.js
@@ -0,0 +1,53 @@
+
+
+var donaciones = [5,15,22,25,30,52,5,1,0];
+
+// Esta funcion debe retornar el monto mas bajo de la lista de montos
+function minimo(montos) {
+   var min = montos[0];
+
+   for (var i = 0; i < montos.length; i++) {
+      /* Si el monto actual es menor que el minimo actual, 
+      entonces el monto actual es el nuevo minimo*/
+      if (montos[i] < min) {
+         min = montos[i];
+      }
+   }
+
+   return min;
+}
+
+// Esta funcion debe retornar el monto mas alto de la lista de montos
+function maximo(montos) {
+   var max = montos[0];
+
+   for (var i = 0; i < montos.length; i++) {
+      /* Si el monto actual es mayor que el maximo actual, 
+      entonces el monto actual es el nuevo maximo*/
+      if (montos[i] > max) {
+         max = montos[i];
+      }
+   }
+
+   return max;
+}
+
+function promedio(min, max,donaciones) {
+   var sum = 0;
+   var cantidad = 0;
+
+   for (monto of donaciones){
+       if (monto > min && monto < max){
+           sum += monto;
+           cantidad++;
+       }
+   }
+
+   return sum / cantidad;
+ }
+
+
+
+console.log("El monto mas bajo es: " + minimo(donaciones));
+console.log("El monto mas alto es: " + maximo(donaciones));
+console.log("El promedio de donaciones es: " + promedio(minimo(donaciones), maximo(donaciones), donaciones));
