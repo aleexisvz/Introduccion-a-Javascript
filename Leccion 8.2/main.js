7a442b5736f8905dd1444cccb5ef2252ceab1cf9
Alexis Vazquez <alexiis1998@hotmail.com>
2023-01-31 23:16:41 -0300

    add: comentarios

--- Leccion 8.2/main.js
+++ Leccion 8.2/main.js
@@ -1,11 +1,13 @@
+
+// Realiza una gestion entre 2 numeros
 function GestionarEstudiante (estudiante, nota1, nota2, Gestion){
-   var resGestion = Gestion(nota1,nota2);
+   var resGestion = Gestion(nota1, nota2);
    console.log ('Nombre Completo del Estudiante: ' + estudiante + ' ' + resGestion);
 }
 
-function Promedio (nota1,nota2){
+function Promedio (nota1,nota2){    // Gestion
    return (nota1+nota2)/2;
 }
 
-
-GestionarEstudiante('Jose Carrillo', 18, 20, Promedio);
+// Comprobamos la funcion
+GestionarEstudiante('Jose Carrillo', 18, 20, Promedio);     // Nombre Completo del Estudiante: Jose Carrillo 19
